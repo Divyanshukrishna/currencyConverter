825e5acf35cc837a756e796d95bc78de4c5679c4
DIVYANSHU_KRISHNA <131163849+Divyanshukrishna@users.noreply.github.com>
2023-11-17 13:30:39 +0530

    git commit

---
+++ currencyConverter/index.js
@@ -0,0 +1,15 @@
+const dropList= document.querySelectorAll(".drop-list select");
+
+for(let i=0;i<drop-List.length;i++){
+    console.log(currency_code)
+    // for(currency_code in country_code){
+    //     let selected;
+    //     if(i==0){
+    //         selected= currency_code=="IND"? "selected": "";
+    //     }
+
+    //    let  optionTag='<option value="${currency_code}"> ${currency_code} </option>';
+    //    dropList[i].insertAdjacentHTML("beforeend", optionTag );
+    // }
+}
+ 
